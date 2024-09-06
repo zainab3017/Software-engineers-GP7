4dc0b6ae06d7a16881f43d2108498fc58f5e9e2f
zainab3017 <zainab3017@gmail.com>
2024-09-06 22:19:37 +0400

    adding registration form files

---
+++ ZainabAlhammadi/RegistrationForm/php/config.php
@@ -0,0 +1,21 @@
+	<?php
+// Define the Gmail smtp server
+define('MAILHOST', "smtp.gmail.com");
+ 
+// Define as a username the email that you use in your Gmail account.
+define('USERNAME', "hhhassanhafeez91@gmail.com");
+ 
+// Define your 16 digit Gmail app-password.
+define('PASSWORD', "bvricfmombchkcia");
+ 
+// Define the email address from which the email is sent.
+define('SEND_FROM', "hhhassanhafeez91@gmail.com");
+ 
+// Define the name of the website from which the email is sent. 
+define('SEND_FROM_NAME', "AIBYAN");
+ 
+// Define the reply-to address.
+define('REPLY_TO', "hhhassanhafeez91@gmail.com");
+ 
+// Define the reply-to name.
+define('REPLY_TO_NAME', "AIBYAN");
