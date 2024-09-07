0c3cd36d9b1b53ab70d61e2c2ac2fa8df0ee7195
zainab3017 <zainab3017@gmail.com>
2024-09-07 19:43:48 +0400

    modify php config file

--- ZainabAlhammadi/RegistrationForm/php/config.php
+++ ZainabAlhammadi/RegistrationForm/php/config.php
@@ -3,19 +3,19 @@
 define('MAILHOST', "smtp.gmail.com");
  
 // Define as a username the email that you use in your Gmail account.
-define('USERNAME', "zainab3017@gmail.com");
+define('USERNAME', "zainabalhammadi3017@gmail.com");
  
 // Define your 16 digit Gmail app-password.
-define('PASSWORD', "bvricfmombchkcia");
+define('PASSWORD', "omms irgu ctuk mzph");
  
 // Define the email address from which the email is sent.
-define('SEND_FROM', "zainab3017@gmail.com");
+define('SEND_FROM', "zainabalhammadi3017@gmail.com");
  
 // Define the name of the website from which the email is sent. 
 define('SEND_FROM_NAME', "ALBYAN");
  
 // Define the reply-to address.
-define('REPLY_TO', "zainab3017@gmail.com");
+define('REPLY_TO', "zainabalhammadi3017@gmail.com");
  
 // Define the reply-to name.
 define('REPLY_TO_NAME', "ALBYAN");
